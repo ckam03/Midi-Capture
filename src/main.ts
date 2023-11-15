ef32e9ceab72d6ce83e34f422756689b159cb8e7
chris kamer <cwkamer@gmail.com>
2023-11-15 18:56:41 -0500

    removed top level await

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import "./style.css";
 import { getMidiDevices } from "./mididevice.ts";
 import { captureMidi } from "./capturemidi.ts";
 
-await getMidiDevices();
+getMidiDevices().then();
 captureMidi();
