fffaa4d7ec163575947b7ae56a31e10415bd86e5
chris kamer <cwkamer@gmail.com>
2023-11-15 19:46:23 -0500

    Added build target in vite config

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import "./style.css";
 import { getMidiDevices } from "./mididevice.ts";
 import { captureMidi } from "./capturemidi.ts";
 
-getMidiDevices().then();
+await getMidiDevices();
 captureMidi();
